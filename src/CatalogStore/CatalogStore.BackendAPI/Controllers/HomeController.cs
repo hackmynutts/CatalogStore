f86efcb79fb3c3229619d01264ceb27da072c93a
hackmynutts <jorgemoruagonzalez@gmail.com>
2026-08-10 23:28:06 -0600

    BASE STRUCTURE FOR BACKEND API

---
+++ src/CatalogStore/CatalogStore.BackendAPI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogStore.BackendAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
